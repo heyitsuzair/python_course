0864a5d17c665ad92978627c057679507cefe3b4
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-19 05:56:41 +0500

    Variables And Data Type

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('Hello', 5)
-print(5)
+a = "uzair"
+b = complex(9, 2)
+c = True
+d = None
+e = [9, 10, 11]
+f = {"name": "Uzair", "age": 20}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
