f86a2acd6f0d451011f041b37884b21a81e82356
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-20 03:12:51 +0500

    If Else

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-name = '! Uzair Uzair !'
-print(name.upper())
-print(name.lower())
-print(name.rstrip('!'))
-print(name.lstrip('!'))
-print(name.replace('Uzair', 'Muhammad Uzair'))
-print(name.split(' '))
-intro = 'my Name Is Uzair'
-print(intro.capitalize())
-str1 = 'Welcome To Console'
-print(len(str1))
-print(len(str1.center(35)))
-print(name.count('Uzair'))
-print(name.endswith('Uzair !'))
-print(name.find('Uzair'))
-str2 = 'Uzair Is A Good boy'
-print(str2.isalnum())
-str3 = 'uzair\n'
-print(str3.isalpha())
-print(str3.islower())
-print(str3.isprintable())
-str4 = '  '
-print(str4.isspace())
-print(str2.istitle())
-print(str2.title())
+a = int(input('Please Enter Age:'))
+if (a < 18):
+    print('You Cannot Drive')
+else:
+    print('You Can Drive')
