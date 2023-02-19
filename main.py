47bcc27d05676d3b420aaede29f9d9abdf6831fb
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-20 02:27:17 +0500

    String Methods

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-names = 'Uzair,Ahmed'
-print(names[0:4])
+name = '! Uzair Uzair !'
+print(name.upper())
+print(name.lower())
+print(name.rstrip('!'))
+print(name.lstrip('!'))
+print(name.replace('Uzair', 'Muhammad Uzair'))
+print(name.split(' '))
+intro = 'my Name Is Uzair'
+print(intro.capitalize())
+str1 = 'Welcome To Console'
+print(len(str1))
+print(len(str1.center(35)))
+print(name.count('Uzair'))
+print(name.endswith('Uzair !'))
+print(name.find('Uzair'))
+str2 = 'Uzair Is A Good boy'
+print(str2.isalnum())
+str3 = 'uzair\n'
+print(str3.isalpha())
+print(str3.islower())
+print(str3.isprintable())
+str4 = '  '
+print(str4.isspace())
+print(str2.istitle())
+print(str2.title())
