4ab052a875db6fc66b4e0568424f37c3aa37c56f
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-21 01:48:56 +0500

    Function Arguments

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def calculateGmean(a, b):
-    gmean = (a*b)/(a+b)
-    return gmean
+# def average(a=2, b=2):
+#     print('Average Is', (a+b)/2)
 
 
-print(calculateGmean(1, 2))
-print(calculateGmean(4, 10))
+# average(b=9)
+
+
+# def greet(fname, lname):
+#     print('Hello', fname, lname)
+
+
+# greet('Muhammad', 'Uzair')
+
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    return sum/len(numbers)
+
+
+avg = average(5, 5)
+print(avg)
