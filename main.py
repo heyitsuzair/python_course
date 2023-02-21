adfd8932bc5379db4b79d0f61f3b862ebf661d4b
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-22 00:57:10 +0500

    Finally

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-a = input('Enter A Number:')
-print(f"Multiplication Of {a} is:")
 try:
-    for i in range(1, 11):
-        print(f"{int(a)} X {i}={int(a) * i}")
-except ValueError:
-    print('Invalid Input')
-except IndexError:
-    print('Index Error')
+    l = [1, 4, 6, 7]
+    i = int(input('Enter A Index:'))
+    print(l[i])
+except:
+    print('Something went wrong')
+finally:
+    print('I am always executed')
