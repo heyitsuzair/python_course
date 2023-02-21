9b3b6a02ef441249e326ab95ef5670e8524f0532
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-22 00:19:56 +0500

    For With Else

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-ep1 = {
-    122: 45,
-    123: 89,
-    124: 65,
-    670: 65
-}
-ep2 = {167: 90, 150: 45}
-# ep1.update(ep2)
-# ep1.clear()
-# ep1.pop(122)
-# ep1.popitem()
-del ep1[122]
-print(ep1)
+for i in range(10):
+    print(i)
+else:
+    print('Loop Ended')
