a96e38f9ba772f9a93c8cca55fea35c962fa86a9
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-22 02:47:34 +0500

    Custom Errors

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-try:
-    l = [1, 4, 6, 7]
-    i = int(input('Enter A Index:'))
-    print(l[i])
-except:
-    print('Something went wrong')
-finally:
-    print('I am always executed')
+a = int(input('Enter Any Value Between 5 And 9:'))
+
+if (a < 5 or a > 9):
+    raise Exception('Value must be between 5 and 9')
