e0144df224fdb632deacd8d4153fcb062509713d
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-21 05:57:43 +0500

    Sets

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-letter = 'Hey My Name Is {1} and I Am From {0}'
-country = 'Pakistan'
-name = 'Uzair'
-
-letter.format(name, country)
-print(f"Hey My Name is {name} and I am from {country}")
+s={2,3,4,5,5}
+print(len(s))
+print(s)
+for i in s:
+    print(i)
