72b62195ceb924436962100874f4398976a124be
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-23 02:46:33 +0500

    Map Filter And Reduce

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# def double(x):
-#     return x*2
 
-# def apply(fx,value):
-#     return 5+fx(value)
 
-# double= lambda x,y:x*y
-# cube= lambda x:(x**3)
-# avg= lambda x,y:(x+y)/2
+from functools import reduce
+l = [1, 2, 3, 4, 5]
 
-# print(double(5,10))
-# print(cube(5))
-# print(avg(5,10))
-# print(apply(lambda x:x**2,5))
+newl = list(map(lambda x: x**3, l))
+print(newl)
+
+newnewl = list(filter(lambda a: a > 4, l))
+print(newnewl)
+
+
+n = [1, 2, 3, 4]
+
+sum = reduce(lambda x, y: x + y, n)
+print(sum)
