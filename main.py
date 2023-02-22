a001b8965f441c8da7de943c3ea79a418f6da132
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-23 02:55:12 +0500

    Difference In is And ==

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
+a = None
+b = None
 
-
-from functools import reduce
-l = [1, 2, 3, 4, 5]
-
-newl = list(map(lambda x: x**3, l))
-print(newl)
-
-newnewl = list(filter(lambda a: a > 4, l))
-print(newnewl)
-
-
-n = [1, 2, 3, 4]
-
-sum = reduce(lambda x, y: x + y, n)
-print(sum)
+print(a == b)
+print(a is b)
