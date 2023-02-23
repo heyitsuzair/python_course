4874f6c6289be74b5d166f75f0579e47252a17ca
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-24 01:34:06 +0500

    super Keyword

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+# class Parent:
+#     def parent_method(self):
+#         print('This Is Parent Method')
+
+
+# class Child(Parent):
+#     def parent_method(self):
+#         print('This Is parent Method')
+#         super().parent_method()
+
+#     def child_method(self):
+#         print('This Is Child Method')
+#         super().parent_method()
+
+
+# child_object = Child()
+# child_object.child_method()
+# child_object.parent_method()
+
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self, name, id):
         self.name = name
-        self.salary = salary
-
-    @classmethod
-    def from_str(cls, string):
-        return cls(string.split('-')[0], int(string.split('-')[1]))
-
-# e = Employee('Uzair', 10000)
-# print(e.name)
-# print(e.salary)
+        self.id = id
 
 
-string = 'Uzair-12000'
-e = Employee.from_str(string)
-print(e.name)
-print(e.salary)
+class Programmer(Employee):
+    def __init__(self, name, id, lang):
+        self.lang = lang
+        super().__init__(name,id)
+
+
+a = Employee('Uzair', "420")
+b = Programmer('Ahmed', '202', 'Python')
+print(b.name)
+print(b.id)
+print(b.lang)
