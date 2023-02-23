3e41196d318b9f379289ee022aa967c13d474d0f
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-24 03:22:08 +0500

    Method Overriding

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from emp import Employee
+class Shape:
 
-e = Employee()
-print(str(e))
-print(repr(e))
-e()
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x * self.y
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        super().__init__(radius, radius)
+
+    def area(self):
+        return 3.14 * super().area()
+
+
+# rec = Shape(3, 4)
+# print(rec.area())
+
+c = Circle(5)
+print(c.area())
