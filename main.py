2399bb31fdf72b73a9e87a77c8746a5692152d85
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-23 20:32:13 +0500

    Access Modifiers

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 class Employee:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+    def __init__(self):
+        self.__name = 'Uzair'  # "__" for private
 
-    def show_details(self):
-        print(f"Name Of Employee Is {self.name} And ID Is {self.id}")
+    def greet(self):
+        print(f"Hey {self.__name}")
 
-class Programmer(Employee):
-    def show_lang(self):
-        print(f"Default Language Is Python")
 
-emp = Employee('Uzair', 402)
-emp.show_details()
-
-emp2=Programmer('Ahmed',101)
-emp2.show_lang()
+a = Employee()
+a.greet()
+print(a._Employee__name)
