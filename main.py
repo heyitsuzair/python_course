5440cccba4c2de71d2dfbc18ac7aa7ff7799b3fa
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-24 06:55:27 +0500

    Time Module

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import win32com.client as wincom
+import time
 
 
-speak = wincom.Dispatch("SAPI.SpVoice")
+# def usingWhile():
+#     i = 0
+#     while i < 5000:
+#         i += 1
+#         print(i)
 
-l=['Ahmed','Uzair','Ali']
 
-for name in l:
-    text = f"Shoutout To {name}"
-    speak.Speak(text)
-    
+# def usingFor():
 
+#     for i in range(5000):
+#         i += 1
+#         print(i)
+
+
+# init = time.time()
+# usingFor()
+# print(time.time()-init)
+# usingWhile()
+# print(time.time()-init)
+
+print(4)
+time.sleep(3)
+print('Printed After 3 seconds')
