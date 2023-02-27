5577fb939fcc1b4f979b4d764a3098083eb45512
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-27 06:48:15 +0500

    Generators

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-import requests
-# url = 'https://jsonplaceholder.typicode.com/posts'
-from bs4 import BeautifulSoup
-
-# response = requests.get('https://google.com')
-# print(response.text)
-
-# data = {
-#     "userId": "12",
-#     "title": "hello2",
-#     "body": "fmpfefefpmep2"
-# }
-
-# response = requests.post(
-#    url , json=data)
-# print(response.text)
+def my_generate():
+    for i in range(5):
+        yield i
 
 
-response = requests.get('https://google.com')
-soup = BeautifulSoup(response.text, 'html.parser')
-
-for img in soup.find_all('img'):
-    print(img['src'])
+gen = my_generate()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
